bdef40352c0626a072920cae291c8b68a41cc4e4
rebecca <rebeccaymchang@gmail.com>
2023-04-19 17:53:57 -0400

    Step 121

--- Codey raceway/Assets/Scripts/TimerrsCountdown.cs
+++ Codey raceway/Assets/Scripts/TimerrsCountdown.cs
@@ -11,12 +11,30 @@ public class TimerrsCountdown : MonoBehaviour
 
     public float totalLapTime;
     public float totalCountdownTime;
+    public CodeyMove Speed;
     void Update()
     {
-        totalLapTime -= Time.deltaTime;
-        totalCountdownTime -= Time.deltaTime;
 
-        laptime.text = Mathf.Round(totalLapTime).ToString();
-        startCountdown.text = Mathf.Round(totalCountdownTime).ToString();
+        if (totalCountdownTime >= 0)
+        {
+            totalCountdownTime -= Time.deltaTime;
+            startCountdown.text = Mathf.Round(totalCountdownTime).ToString();
+            Speed.Speed = 0;
+        }
+
+        if(totalCountdownTime <= 0)
+        {
+            startCountdown.text = ("");
+            totalLapTime -= Time.deltaTime;
+
+            laptime.text = Mathf.Round(totalLapTime).ToString();
+            Speed.Speed = 700;
+        }
+
+        if (totalLapTime < 0)
+        {
+            print("Time is up!");
+        }
+
     }
 }
