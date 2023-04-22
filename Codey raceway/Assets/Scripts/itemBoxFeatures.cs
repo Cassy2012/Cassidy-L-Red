21cfa4707d58a8f849ec38bf7cfaee262613f1df
Cassidy <cassidy.lo78@gmail.com>
2023-04-22 11:32:07 -0400

    Step147

---
+++ Codey raceway/Assets/Scripts/itemBoxFeatures.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class itemBoxFeatures : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.Rotate(Vector3.forward, Time.deltaTime * 30f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
