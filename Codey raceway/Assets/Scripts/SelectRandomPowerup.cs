654145825e53aa00ccb8d47a8cf75a0bf259a861
b <fariskhurram10@gmail.com>
2023-05-06 11:35:05 -0400

    step 174

--- Codey raceway/Assets/Scripts/SelectRandomPowerup.cs
+++ Codey raceway/Assets/Scripts/SelectRandomPowerup.cs
@@ -7,32 +7,36 @@ public class SelectRandomPowerup : MonoBehaviour
     public List<GameObject> powerupList;
     public int randomNumberInList;
     public GameObject chosenPowerup;
-    public bool canSpawn = true;
+    public bool canSpawn = false;
 
     public int modifyXPosition;
     public int modifyZPosition;
     void Start()
     {
+
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("Fire1"))  
+        if (Input.GetButtonDown("Fire1") && canSpawn)
         {
 
-            canSpawn = true;
-                GameObject chosenPowerupClone = Instantiate(chosenPowerup, new Vector3(transform.position.x + modifyXPosition , transform.position.y, transform.position.z + modifyZPosition ), transform.rotation);
+            Debug.Log("Powerup");
+            canSpawn = false;
+            GameObject chosenPowerupClone = Instantiate(chosenPowerup, new Vector3(transform.position.x + modifyXPosition , transform.position.y, transform.position.z + modifyZPosition ), transform.rotation);
             
             print("Powerup");
         }
     }
 
-    private void OnCollisionEnter(Collision other)
+    private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "itemBoxes")
         {
+            print("PickedUp");
+            canSpawn = true;
             randomNumberInList = Random.Range(0, powerupList.Count);
             chosenPowerup = powerupList[randomNumberInList];
         }
