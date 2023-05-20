d3c4fe4387eb2bf6dee24f9bbc8ea3937e4e6fa3
Cassidy <cassidy.lo78@gmail.com>
2023-05-20 11:30:45 -0400

    Step181

--- Codey raceway/Assets/Scripts/NavMeshMovement.cs
+++ Codey raceway/Assets/Scripts/NavMeshMovement.cs
@@ -9,23 +9,40 @@ public class NavMeshMovement : MonoBehaviour
 
     Animator animator;
 
-    NavMeshAgent Agent;
+    private NavMeshAgent Agent;
     // Start is called before the first frame update
-    private void Start()
+     private void Start()
     {
-        GameObject.FindGameObjectWithTag("Objects").transform;
+        GameObject.FindGameObjectsWithTag("Objects");
+        Agent = GetComponent<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
 
+        GetComponent<NavMeshAgent>().SetDestination(goal.transform.position);
+        goal = GameObject.FindGameObjectWithTag("Objects").transform;
+        Agent = GetComponent<NavMeshAgent>();
 
+        Agent.destination = goal.transform.position;
+        Debug.Log(Agent.destination);
     }
 
     // Update is called once per frame
     void Update()
     {
         
-        //  transform.position += transform.forward * Time.deltaTime * 10;
-        animator = GetComponentInChildren<Animator>();
-        Agent = GetComponent<NavMeshAgent>();
-        Agent.destination = goal.position;
+        transform.position += transform.forward * Time.deltaTime * 10;
+       // animator = GetComponentInChildren<Animator>();
+        //Agent = GetComponent<NavMeshAgent>();
+       // Debug.Log(transform.position);
+       // Agent.SetDestination (goal.position);
+        
+    }
 
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "Objects")
+        {
+            Destroy(gameObject);
+        }
     }
 }
