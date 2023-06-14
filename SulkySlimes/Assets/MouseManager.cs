3fcb4de76f6f8f58da1f90c4fa77373eb2ded822
Alexander <markhamonca@codeninjas.com>
2023-06-14 17:53:46 -0400

    Step 29

---
+++ SulkySlimes/Assets/MouseManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseManager : MonoBehaviour
+{
+    [Header("Mouse Info")]
+    public Vector3 clickStartLocation;
+
+    [Header("Physics")]
+    public Vector3 launchVector;
+    public float launchForce;
+
+    [Header("Slime")]
+    public Transform slimeTransform;
+    public Rigidbody slimeRigidbody;
+
+    
+
+
+    // Update is called once per frame
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            clickStartLocation = Input.mousePosition;
+        }
+
+
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 mouseDiffrence = clickStartLocation - Input.mousePosition;
+            launchVector = new Vector3(
+                mouseDiffrence.x * 1f,
+                mouseDiffrence.y * 1.2f,
+                mouseDiffrence.y * 1.5f
+                );
+            launchVector.Normalize();
+        }
+
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            print("Release");
+        }
+    }
+}
