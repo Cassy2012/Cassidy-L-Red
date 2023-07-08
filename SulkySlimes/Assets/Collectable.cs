490e0da055c027be2d7ba533b78b75ccb6b306f3
Aroosh <handexter000@gmail.com>
2023-07-08 11:55:24 -0400

    step 41

--- SulkySlimes/Assets/Collectable.cs
+++ SulkySlimes/Assets/Collectable.cs
@@ -4,25 +4,39 @@ using UnityEngine;
 
 public class Collectable : MonoBehaviour
 {
-    public float distanceToMove;
+    public float distanceToMove = 40;
 
     
     private Vector3 startingPosition;
     private Vector3 endingPosition;
 
-    public Transform SphereTransform;
-    public Transform SphereValue;
+    public float speed = 0.1f;
+    public float direction = -1f;
+
     // Start is called before the first frame update
     void Start()
     {
-        startingPosition = SphereTransform.position;
-        endingPosition = SphereValue.position;
+        startingPosition = transform.position;
+        endingPosition = startingPosition + new Vector3(distanceToMove,0, 0);
+            
+
+       
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = (direction * speed * Time.deltaTime * +, 0, 0); 
+        if (transform.position.x < startingPosition.x)
+        {
+            direction = 1;
+        }
+
+        if(transform.position.x > endingPosition.x)
+        {
+            direction = -1;
+        }
+      
     }
 }
