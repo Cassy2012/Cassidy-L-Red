dedd87cecdc034296e2db645be0247e6343933c8
Cassidy <cassidy.lo78@gmail.com>
2023-07-12 17:55:28 -0400

    Step 43

--- SulkySlimes/Assets/Collectable.cs
+++ SulkySlimes/Assets/Collectable.cs
@@ -4,39 +4,53 @@ using UnityEngine;
 
 public class Collectable : MonoBehaviour
 {
-    public float distanceToMove = 40;
 
+    // A longer example of Vector3.Lerp usage.
+    // Drop this script under an object in your scene, and specify 2 other objects in the "startMarker"/"endMarker" variables in the script inspector window.
+    // At play time, the script will move the object along a path between the position of those two markers.
     
-    private Vector3 startingPosition;
-    private Vector3 endingPosition;
+        // Transforms to act as start and end markers for the journey.
+        public Transform startMarker;
+        public Transform endMarker;
 
-    public float speed = 0.1f;
-    public float direction = -1f;
+    // Movement speed in units per second.
+    public float speed = 1.0F;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        startingPosition = transform.position;
-        endingPosition = startingPosition + new Vector3(distanceToMove,0, 0);
-            
+        // Time when the movement started.
+        private float startTime;
 
-       
+        // Total distance between the markers.
+        private float journeyLength;
 
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        transform.position = (direction * speed * Time.deltaTime * +, 0, 0); 
-        if (transform.position.x < startingPosition.x)
+    public AnimationCurve curve;
+    public float time;
+ 
+
+        void Start()
         {
-            direction = 1;
+            // Keep a note of the time the movement started.
+            startTime = Time.time;
+
+            // Calculate the journey length.
+            journeyLength = Vector3.Distance(startMarker.position, endMarker.position);
         }
 
-        if(transform.position.x > endingPosition.x)
+        // Move to the target end position.
+        void Update()
         {
-            direction = -1;
-        }
-      
+            // Distance moved equals elapsed time times speed..
+            float distCovered = (Time.time - startTime) * speed;
+
+            // Fraction of journey completed equals current distance divided by total distance.
+            float fractionOfJourney = distCovered / journeyLength;
+
+        // Set our position as a fraction of the distance between the markers.
+        //transform.position = Vector3.Lerp(startMarker.position, endMarker.position, fractionOfJourney);
+        time += Time.deltaTime;
+        transform.position = Vector3.Lerp(startMarker.position, endMarker.position, curve.Evaluate(time));
+        
+         //transform.position = Vector3.Lerp(endMarker.position, startMarker.position, fractionOfJourney);
+
     }
 }
